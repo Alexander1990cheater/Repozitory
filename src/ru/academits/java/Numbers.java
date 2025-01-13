b099a316e92f85b9aac07a54bd70edb2598b95a2
Alexander1990cheater <139058903+Alexander1990cheater@users.noreply.github.com>
2025-01-13 17:07:18 +0300

    13.01.2025

---
+++ src/ru/academits/java/Numbers.java
@@ -0,0 +1,11 @@
+package ru.academits.java;
+
+import java.util.Scanner;
+
+public class Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите целое число");
+        int a = Integer.parseInt(scanner.nextLine());
+    }
+}
